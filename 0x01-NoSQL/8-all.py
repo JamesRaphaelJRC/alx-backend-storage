5a11c54d08836ff107438bdc6f04619b1b8af9ae
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-21 20:53:02 +0100

    Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 ''' Defines a function '''
+from pymongo import MongoClient
+from typing import Iterator
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection: MongoClient) -> Iterator:
     ''' Lists all documents in a mongoDB collection 
 
         mongo_collection: The collection ( a pymongo collection object )
