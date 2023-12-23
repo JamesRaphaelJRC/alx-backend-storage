2c7ac20d0d44466a85124c7978de98e8c94dd651
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-23 19:41:58 +0100

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def provide_stats():
+    ''' Provides some stats about NGINX logs stored in MongoDB '''
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+
+    number_of_logs = nginx_collection.count_documents({})
+
+    num_GET_method = nginx_collection.count_documents({"method": "GET"})
+    num_POST_method = nginx_collection.count_documents({"method": "POST"})
+    num_PUT_method = nginx_collection.count_documents({"method": "PUT"})
+    num_PATCH_method = nginx_collection.count_documents({"method": "PATCH"})
+    num_DELETE_method = nginx_collection.count_documents({"method": "DELETE"})
+
+    num_GET_path_status = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+
+    output = f"{number_of_logs} logs\n\
+Methods:\n\
+    \tmethod GET: {num_GET_method}\n\
+    \tmethod POST: {num_POST_method}\n\
+    \tmethod PUT: {num_PUT_method}\n\
+    \tmethod PATCH: {num_PATCH_method}\n\
+    \tmethod DELETE: {num_DELETE_method}\n\
+{num_GET_path_status} status check\
+    "
+    print(output)
+
+
+if __name__ == '__main__':
+    provide_stats()
